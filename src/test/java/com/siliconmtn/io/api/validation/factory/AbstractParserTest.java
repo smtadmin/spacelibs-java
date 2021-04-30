2732332dbf614e37e7376a1d9373adcfe8ae3064
balasmt <Bala.Bugatha@siliconmtn.com>
2021-04-30 15:11:44 -0600

    test(abstract parser): added missing tests and access modifiers

--- src/test/java/com/siliconmtn/io/api/validation/factory/AbstractParserTest.java
+++ src/test/java/com/siliconmtn/io/api/validation/factory/AbstractParserTest.java
@@ -3,13 +3,20 @@ package com.siliconmtn.io.api.validation.factory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.verify;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import com.siliconmtn.io.api.validation.factory.AbstractParser.AttributeKey;
+import com.siliconmtn.io.api.validation.validator.ValidationDTO;
+import com.siliconmtn.io.api.validation.validator.ValidatorIntfc.ValidatorType;
 
 /****************************************************************************
  * <b>Title:</b> AbstractParserTest.java
@@ -27,10 +34,31 @@ import com.siliconmtn.io.api.validation.factory.AbstractParser.AttributeKey;
 class AbstractParserTest {
 	
 	// Members
-	protected AbstractParser parser = new TestParser();
+	
+	protected AbstractParser parser = Mockito.spy(TestParser.class);
 	protected Map<AttributeKey, Object> attributes = new EnumMap<>(AttributeKey.class);
+	private ValidationDTO validationDTO;
+	private List<ValidationDTO> validationDTOList;
+	@BeforeEach
+	void init()
+	{	
 
-	/**
+		validationDTO = ValidationDTO
+		.builder()
+		.value("hello")
+		.type(ValidatorType.STRING)
+		.validOptions(null)
+		.alternateValidationId(false)
+		.min("0")
+		.max("10")
+		.build();
+		
+		validationDTOList = new ArrayList<>();
+		validationDTOList.add(validationDTO);
+		
+	}
+	
+	/*
 	 * Test method for {@link com.siliconmtn.io.api.validation.factory.AbstractParser#AbstractParser()}.
 	 */
 	@Test
@@ -100,6 +128,23 @@ class AbstractParserTest {
 	@Test
 	void testRequestParser() throws Exception {
 		assertEquals(0, parser.requestParser(null).size());
+	}
+	
+	/*
+	 * Test method for {@link com.siliconmtn.io.api.validation.factory.AbstractParser#build(ValidatorType, String, Map<String, String>, boolean, String, String)}.
+	 */
+	@Test
+	void testBuild() {
+		assertEquals(validationDTO, parser.build(ValidatorType.STRING,"hello",null, false, "10","0"));
+	}
+	
+	/*
+	 * Test method for {@link com.siliconmtn.io.api.validation.factory.AbstractParser#addDTOToList(List<ValidationDTO>, ValidationDTO)}.
+	 */
+	@Test
+	void testAddDTOToList() {
+		parser.addDTOToList(validationDTOList, validationDTO);
+		verify(parser).addDTOToList(validationDTOList, validationDTO);
 	}
 
 }

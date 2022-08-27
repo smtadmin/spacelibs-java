806134f043c7fe55676ab12ba0a92ac92571ffbf
smt-etewa <james@siliconmtn.com>
2022-08-27 13:31:38 -0600

    Cleaned docs

--- src/test/java/com/siliconmtn/io/aws/AWSS3FileManagerTest.java
+++ src/test/java/com/siliconmtn/io/aws/AWSS3FileManagerTest.java
@@ -14,8 +14,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-import java.io.ByteArrayInputStream;
 // JDK 11.x
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.Instant;

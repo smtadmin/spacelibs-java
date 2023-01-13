b595f0498374d3c7a1ef23429d888a35dea3f0ec
William R Larsen Jr <billy@siliconmtn.com>
2023-01-13 18:38:07 -0500

    Added Eventing code

---
+++ src/main/java/com/siliconmtn/data/event/AbstractEventManager.java
@@ -0,0 +1,156 @@
+package com.siliconmtn.data.event;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+/****************************************************************************
+ * <b>Title:</b> AbstractEventManager.java <br>
+ * <b>Project:</b> spacelibs-java <br>
+ * <b>Description:</b> Abstract handling all the base implementation of the interface.
+ * Handles setup, reset and teardown of timers and integrates checks for event sending<br>
+ * <b>Copyright:</b> Copyright (c) 2023 <br>
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Billy Larsen
+ * @version 1.x
+ * @since Jan 13, 2023
+ *        <b>updates:</b>
+ * 
+ ***************************************************************************
+ */
+@Component
+public abstract class AbstractEventManager<T extends Comparable<T>> implements EventManagerIntfc<T>{
+
+	//Internal SpringBoot Eventing System that is leveraged for Observables. 
+    protected ApplicationEventPublisher applicationEventPublisher;
+
+    //Holds the Data for validation/processing
+    protected Collection<T> data; 
+
+    //Timeout duration in milliseconds.  -1 is disabled.
+	protected long timeoutInMilliseconds = -1;
+
+	//Holds the Timer Object for internal usage.
+	protected Timer timer;
+
+	//Default Constructor
+	protected AbstractEventManager(@NotNull ApplicationEventPublisher applicationEventPublisher) {
+		this(applicationEventPublisher, null, -1);
+	}
+
+	//Default Constructor with predefined collection framework.
+	protected AbstractEventManager(@NotNull ApplicationEventPublisher applicationEventPublisher, @NotNull Collection<T> data) {
+		this(applicationEventPublisher, data, -1);
+	}
+
+	//Default Constructor with timeout support.
+	protected AbstractEventManager(@NotNull ApplicationEventPublisher applicationEventPublisher, long timeoutInMilliseconds) {
+		this(applicationEventPublisher, null, timeoutInMilliseconds);
+	}
+
+	//Root All Args Constructor for setting collection and timeout.
+	protected AbstractEventManager(@NotNull ApplicationEventPublisher applicationEventPublisher, Collection<T> data, long timeoutInMilliseconds) {
+		this.applicationEventPublisher = applicationEventPublisher;
+		if(data == null && queueComparator() != null) {
+			this.data = new PriorityQueue<>(queueComparator());
+		} else if(data == null) {
+			this.data = new ArrayList<>();	
+		} else {
+			this.data = data;
+		}
+
+		this.timeoutInMilliseconds = timeoutInMilliseconds;
+	}
+
+	@Override
+	public Collection<T> getData() {
+		return data;
+	}
+
+	/**
+	 * Adds the given element to the collection, resets the timeout timer and attempts to run validation on the collection.
+	 */
+	@Override
+	public void addData(T element) {
+		data.add(element);
+		setTimer();
+		if(validateData()) {
+			sendEvent(true, false);
+		}
+	}
+
+	/**
+	 * Optional Comparator implementation for handling ordered/priority data. 
+	 */
+	@Override
+	public Comparator<T> queueComparator() {
+		return (T ele1, T ele2) -> ele1.compareTo(ele2);
+	}
+
+	/**
+	 * Update the timeout and reset timer.
+	 */
+	@Override
+	public void setTimeout(long timeoutInMilliseconds) {
+		this.timeoutInMilliseconds = timeoutInMilliseconds;
+		setTimer();
+	}
+
+	/**
+	 * If the data exists in collection, removes it and resets timer.
+	 */
+	@Override
+	public void removeData(T element) {
+		if(data.contains(element)) {
+			data.remove(element);
+			setTimer();
+		}
+	}
+
+	/**
+	 * Returns a TimerTask implementation that by default sends a timeout fail
+	 * event.
+	 */
+	@Override
+	public TimerTask getTimeoutTask() {
+		return new TimerTask() {
+			@Override
+			public void run() {
+				sendEvent(false, true);
+			}
+		};
+	}
+
+	/**
+	 * Cancels any running timer and sends an event with success and timeout
+	 * status through the event framework.
+	 */
+	@Override
+	public void sendEvent(boolean isSuccessful, boolean isTimeout) {
+		timer.cancel();
+		applicationEventPublisher.publishEvent(new PublisherEvent<>(this, isSuccessful, isTimeout, getData()));
+	}
+
+	/**
+	 * Internal function for managing lifecycle of the timer.
+	 */
+	private void setTimer() {
+		if(timer != null) {
+			timer.cancel();
+		}
+
+		timer = new Timer();
+		if(this.timeoutInMilliseconds > 0) {
+			this.timer.schedule(getTimeoutTask(), timeoutInMilliseconds);
+		}
+	}
+}
---
+++ src/main/java/com/siliconmtn/data/event/EventManagerIntfc.java
@@ -0,0 +1,75 @@
+package com.siliconmtn.data.event;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.TimerTask;
+
+/**
+ * <b>Title:</b> EventManagerIntfc.java
+ * <b>Project:</b> spacelibs-java
+ * <b>Description:</b> Interface for managing related messaging in a controlled observable way.
+ * This interface defines common high level interactions used for defining and feeding data in to
+ * be managed.  This framework works using the Spring ApplicationEvent System for Alerting observing
+ * code that some work is ready to be performed.
+ *
+ * <b>Copyright:</b> 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author raptor
+ * @version 1.0
+ * @since Nov 22, 2022
+ * @updates
+ *
+ * @param <T>
+ */
+public interface EventManagerIntfc<T extends Comparable<T>> {
+
+	/**
+	 * Add data to collection
+	 * @param element
+	 */
+	public void addData(T element);
+
+	/**
+	 * Remove data from collection
+	 * @param element
+	 */
+	public void removeData(T element);
+
+	/**
+	 * Retrieve data in collection
+	 * @return
+	 */
+	public Collection<T> getData();
+
+	/**
+	 * Validate status of collection.
+	 * @return
+	 */
+	public boolean validateData();
+
+	/**
+	 * Optional Comparator for ordering information in event of sequenced or priority handling.
+	 * @return
+	 */
+	public Comparator<T> queueComparator();
+
+	/**
+	 * Send event through the Eventing System.
+	 * @param valid
+	 * @param timeout
+	 */
+	public void sendEvent(boolean valid, boolean timeout);
+
+	/**
+	 * Set the timeout in milliseconds before executing the related timeoutTask.
+	 * @param timeoutInMilliseconds
+	 */
+	public void setTimeout(long timeoutInMilliseconds);
+
+	/**
+	 * Retrieve the timeout task for what happens on a timeout.
+	 * @return
+	 */
+	public TimerTask getTimeoutTask();
+}
